1c93256c892054e89e4cba3dea8f1354dbfc26d3
Danilo Sampaio <danilo.sampaio@gmail.com>
2024-12-11 16:20:39 -0300

    treat when tree node has not children

--- src/parsers/IntegraphParser.ts
+++ src/parsers/IntegraphParser.ts
@@ -46,10 +46,10 @@ export default class IntegraphParser {
 
     private findIntegraphBlocks(tree: Parser.Tree): IntegraphBlock[] {
         const rootNode = tree.rootNode;
-        const children = rootNode.children;
+        const children = rootNode?.children;
         const integraphBlocks: IntegraphBlock[] = [];
 
-        for (let i = 0; i < children.length; i++) {
+        for (let i = 0; i < children?.length; i++) {
             const currentNode = children[i];
             const commentText = this.nodeHandler.getIntegraphText(currentNode);
             if (commentText) {
