c0b105a9497045d824cf2a09d9e7f0cd80d4cea2
Danilo Sampaio <danilo.sampaio@gmail.com>
2024-12-11 17:27:41 -0300

    fix readFile enconding

--- src/diagrams/__tests__/ArchitectureDiagrama.test.ts
+++ src/diagrams/__tests__/ArchitectureDiagrama.test.ts
@@ -5,7 +5,7 @@ import JavaIntegraphParser from '../../parsers/java/JavaIntegraphParser';
 import { ArchitectureDiagram } from '../architecture';
 
 const loadFixture = (fileName: string) => {
-    return fs.readFile(`src/diagrams/__tests__/fixtures/${fileName}`, { encoding: 'utf-8', flag: 'w+' })
+    return fs.readFile(`src/diagrams/__tests__/fixtures/${fileName}`, { encoding: 'utf8' })
 }
 
 describe('ArchitectureDiagram', () => {
